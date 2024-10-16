66d99553191b98374c388ffe29453d576713da78
PolliX123 <neath5738@gmail.com>
2024-10-16 21:29:19 +0700

    Clean some unused code

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+//import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+    <p>This is my first Vuejs project</p>
+
+</template>
